69c68393b7fb0a9ffed7e4eba640676aad626b3a
abdullah-124 <mdabdullahsakib468@gmail.com>
2022-04-20 13:08:04 +0600

    readme file

--- src/Pages/Register/Firebase/Firebase.config.js
+++ src/Pages/Register/Firebase/Firebase.config.js
@@ -6,12 +6,4 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD243m3qMXqBcUScOzs3gxOa7klcs3oo2c",
-//   authDomain: "bicycle-12.firebaseapp.com",
-//   projectId: "bicycle-12",
-//   storageBucket: "bicycle-12.appspot.com",
-//   messagingSenderId: "124318637810",
-//   appId: "1:124318637810:web:833d32bcc557ed26afba6b",
-// };
 export default firebaseConfig;
